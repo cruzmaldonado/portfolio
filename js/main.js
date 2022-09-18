fd170e55f7e92a2f2ea1d17bd333816fc8a411a0
Cruz Maldonado <cruzmaldonadocharris@gmail.com>
2022-09-17 21:11:17 -0400

    falta acomodar trabajos y corriculum

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(".item").click(function(){
+
+    $(".collapse").collapse("hide");
+    
+  });
+
+  $(document).on("click",'[data-toggle="lightbox"]',function(e){
+    e.preventDefault();
+    $(this).ekkoLightbox();
+  });
